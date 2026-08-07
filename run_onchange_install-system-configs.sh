c2a91e82fb5e1b6f80a2d34ee0a2a2091e665881
João Júnior <jbdsjunior@users.noreply.github.com>
2026-08-07 01:08:55 -0400

    Update and rename run_onchange_install-system-configs.sh to executable_run_onchange_after_install-system-configs.sh.tmpl

--- run_onchange_install-system-configs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Hash dos arquivos para forçar re-execução em caso de mudança (hash automático pelo Chezmoi)
-# system-configs/etc hash: {{ includeTemplate "system-configs/etc" | sha256sum }}
-
-echo "Sincronizando configurações de sistema (/etc)... Será solicitada permissão de administrador."
-sudo rsync -av --chown=root:root "$(chezmoi source-path)/system-configs/etc/" /etc/
-
-echo "Recarregando daemons do systemd para garantir que novos timers/serviços entrem em vigor..."
-sudo systemctl daemon-reload
